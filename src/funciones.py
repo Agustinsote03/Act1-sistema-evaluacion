0ebcbdc41cdb4dab30f3a33ad9e25e3b6d779439
Agustinsote03 <AgustinGhizzo@gmail.com>
2025-09-22 17:51:28 -0300

    Se agregan funciones para evaluacion: calcular_puntaje() y mejor_equipo_de_ronda()

--- src/funciones.py
+++ src/funciones.py
@@ -1,2 +1,16 @@
+def calcular_puntaje(stats): #stats es un diccionario, cuya clave es el nombre del equipo y el valor es otro diccionario con las estadisticas
+    innovacion = 3
+    presentacion = 1
+    puntos = innovacion * stats['innovacion'] + presentacion * stats['presentacion']
+    if stats['errores']:
+     puntos = puntos - 1
+    return puntos
 
+def mejor_equipo_de_ronda(ronda): #ronda es un diccionario
+    puntajes = {} #diccionario vacio
+    for equipo, stats in ronda.items(): #recorro el diccionario, y voy tomando la clave(equipo) y el valor(stats)
+        puntuacion = calcular_puntaje(stats) #Guardo el puntaje en la variable
+        puntajes[equipo] = puntuacion #la clave es el equipo y el valor es la puntuacion
+    mejor_equipo = max(puntajes, key=puntajes.get) #me quedo con la clave del diccionario que tiene el valor mas alto
+    return mejor_equipo
 
